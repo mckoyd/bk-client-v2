97a4afd704bbbe59651c8f24d8cd185f37d32bc2
mckoy <dominick.mckoy@gmail.com>
2018-12-17 15:40:32 -0500

    testing locally

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://bonuskidserver.herokuapp.com'
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/'
